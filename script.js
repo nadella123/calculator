bf0d1b0ae7440fc95ce0346e03bf4810551b65bc
nadella naga sai hasitha <165311153+nadella123@users.noreply.github.com>
2024-03-28 23:28:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let string="";
+let buttonElement=document.querySelectorAll('.buttons');
+Array.from(buttonElement).forEach(buttons =>
+{
+buttons.addEventListener('click',(e)=>
+{
+    if(e.target.innerHTML=='=')
+    {
+        string=eval(string);
+        document.querySelector('input').value=string;
+    }
+    else if(e.target.innerHTML=='C')
+    {
+        string="";
+        document.querySelector('input').value=string;
+    
+    }
+    else if(e.target.innerHTML=='DEL')
+    {
+        
+        string=string.substring(0,string.length-1);
+        document.querySelector('input').value=string;
+    }
+    else{
+        string=string+e.target.innerHTML;
+        document.querySelector('input').value=string;
+    }
+})
+})
